359f82d9a4b1f811c82e21f7a020600db429c871
cmcoke <minoru.cm@gmail.com>
2026-01-01 10:47:02 -0500

    implemented a carousel and popular products section on the home page of the store

--- themes/pawsgang/functions.php
+++ themes/pawsgang/functions.php
@@ -150,6 +150,12 @@ function pawsgang_scripts()
   wp_style_add_data('pawsgang-style', 'rtl', 'replace');
 
   wp_enqueue_script('pawsgang-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+  wp_enqueue_script('bootstrap-popper', 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js', array('jquery'));
+  wp_enqueue_script('bootstrap-script', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js', array('jquery'));
+  wp_enqueue_script('pawsgang-script', get_template_directory_uri() . '/js/script.js', array('jquery'));
+
+
+
 
   if (is_singular() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
