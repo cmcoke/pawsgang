6ad469b52ff65f56c702f5a7896c6ea07f1bad1c
cmcoke <minoru.cm@gmail.com>
2026-01-03 13:50:12 -0500

    fixed bootstrap' carousel error message on the browser' console and edit woocommerce _fonts.scss file so that the review stars appear in the review tab.

--- themes/pawsgang/js/script.js
+++ themes/pawsgang/js/script.js
@@ -1,5 +1,10 @@
-var myCarousel = document.querySelector("#carouselExampleDark");
-var carousel = new bootstrap.Carousel("#myCarousel", {
-  interval: 2000,
-  wrap: false
-});
+// 1. Select the correct element from your HTML
+const myCarouselElement = document.querySelector("#carouselExampleDark");
+
+// 2. Pass that specific element variable into the Carousel constructor
+if (myCarouselElement) {
+  let carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+    wrap: false
+  });
+}
